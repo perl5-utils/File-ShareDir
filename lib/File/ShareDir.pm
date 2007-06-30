bee122731bb6b484720c8a2a911528af88308ec3
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2007-06-30 01:53:31 +0000

    Removing "All rights reserved"
    git-svn-id: http://svn.ali.as/cpan/trunk/File-ShareDir@1788 88f4d9cd-8a04-0410-9d60-8f63309c3137

--- lib/File/ShareDir.pm
+++ lib/File/ShareDir.pm
@@ -342,7 +342,7 @@ L<File::HomeDir>, L<Module::Install>, L<Module::Install::Share>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2005, 2006 Adam Kennedy. All rights reserved.
+Copyright (c) 2005, 2006 Adam Kennedy.
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
 
